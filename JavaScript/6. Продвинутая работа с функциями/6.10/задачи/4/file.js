e450a5687672b56cded9353f5c9968bf4bb6afad
kovalek3011 <mrkv3011@gmail.com>
2023-03-17 16:31:43 +0400

    6.10

---
+++ JavaScript/6. Продвинутая работа с функциями/6.10/задачи/4/file.js
@@ -0,0 +1,32 @@
+/*
+Исправьте функцию, теряющую "this"
+
+Вызов askPassword() в приведённом ниже коде должен проверить пароль 
+и затем вызвать user.loginOk/loginFail в зависимости от ответа.
+
+Однако, его вызов приводит к ошибке. Почему?
+
+Исправьте выделенную строку, чтобы всё работало (других строк изменять не надо).
+*/
+
+function askPassword(ok, fail) {
+  let password = prompt("Password?", "");
+  if (password == "rockstar") ok();
+  else fail();
+}
+
+let user = {
+  name: "Вася",
+
+  loginOk() {
+    alert(`${this.name} logged in`);
+  },
+
+  loginFail() {
+    alert(`${this.name} failed to log in`);
+  },
+};
+
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+
+// ошибка возникала потому что loginOk/loginFail не имеют контекста, метод bind решает эту проблему
